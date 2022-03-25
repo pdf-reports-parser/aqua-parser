8f86495d78e6d58ffc52b76ab88d8d8d87ce8f84
astra <zorkin.roman@gmail.com>
2022-03-25 18:19:04 +0300

    Implement typer feature

--- aqua/__main__.py
+++ aqua/__main__.py
@@ -1,8 +1,23 @@
 import logging
 
+import typer
+
 from aqua import parser
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+
+typer_app = typer.Typer(help='Aqua-Parser manager.')
+
+
+@typer_app.command(help='Stop parser.')
+def stop():
+    typer.echo('Stop parser')
+
+
+@typer_app.command(help='Start parser.')
+def run():
+    parser.parse()
+
 
 if __name__ == '__main__':
-    parser.parse()
+    typer_app()
